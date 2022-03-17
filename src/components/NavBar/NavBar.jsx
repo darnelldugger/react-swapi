9b2a26ef97766bbfa9bd891991f695701207889b
Darnell Dugger <darnelldugger@gmail.com>
2022-03-17 13:26:37 -0400

    create navbar, complete

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return ( 
+    <>
+      <div className="navbar">
+        <header>STAR WARS STARSHIPS</header>  
+      </div>
+    </>
+   );
+}
+ 
+export default NavBar;
